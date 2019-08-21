d93ec933b4542b669db2988baa0191da53d2ba85
dboles42 <44824446+dboles42@users.noreply.github.com>
2019-08-21 14:44:21 -0700

    Working Debug Build
    Working Debug Build
    Updated ADXL 375 Drivers
    Converted to STM32IDE

--- Simon-Debug/Inc/adxl375.h
+++ Simon-Debug/Inc/adxl375.h
@@ -13,18 +13,85 @@
 #endif /* ADXL375_H_ */
 
 
-#include "main.h"
 #include "stm32wb55xx.h"
+#include "main.h"
+/************************************************/
+/* 			ADXL375 REGISTER MAP
+All registers in ADXL375 are 8 bits in length
+	  A5:A0 are the address Bits
+*/
+/************************************************/
+#define REG_DEVID				(0x00) //device ID
+#define REG_THRESH_SHOCK 		(0x1D) //Shock threshold
+#define REG_OFSX 				(0x1E) //X-axis offset
+#define REG_OFSXY 				(0x1F) //Y-axis offset
+#define REG_OFSXZ 				(0x20) //Z-axis offset
+#define REG_DUR 				(0x21) //Shock duration
+#define REG_LATENT 				(0x22) //Shock latency
+#define REG_WINDOW 				(0x23) //Shock window
+#define REG_THRESH_ACT 			(0x24) //Activity threshold
+#define REG_THRESH_INACT 		(0x25) //Inactivity threshold
+#define REG_TIME_INACT 			(0x26) //Inactivity time
+#define REG_ACT_INACT_CTL 		(0x27) //Axis enable control for activity and inactivity detection
+#define REG_SHOCK_AXES 			(0x2A) //Axis control for single shock/double shock
+#define REG_ACT_SHOCK_STATUS	(0x2B) //Source of single shock/double shock
+#define REG_BW_RATE 			(0x2C) //Data rate and power mode control
+#define REG_POWER_CTRL 			(0x2D) //Power saving features control
+#define REG_INT_ENABLE 			(0x2E) //Interrupt enable control
+#define REG_INT_MAP 			(0x2F) //Interrupt mapping control
+#define REG_INT_SOURCE 			(0x30) //Interrupt source
+#define REG_DATA_FORMAT 		(0x31) //Data format control
+#define REG_DATAX0 				(0X32) //X-Axis Data LH
+#define REG_DATAX1 				(0X33) //X-Axis Data UH
+#define REG_DATAY0 				(0X34) //Y-Axis Data LH
+#define REG_DATAY1 				(0X35) //Y-Axis Data UH
+#define REG_DATAZ0 				(0X36) //Z-Axis Data LH
+#define REG_DATAZ1 				(0X37) //Z-Axis Data UH
+#define REG_FIFO_CTL			(0X38) //FIFO control
+#define FIFO_STATUS 			(0X39) //FIFO status
+
+/************************************************/
+/* 		ADXL375 COMMON REGISTER VALUES
+/************************************************/
+
+//Sampling Rate values
+#define BW_3200HZ      (0x0F) //3200HZ
+#define BW_1600HZ      (0x0E) //1600HZ
+#define BW_800HZ       (0x0D) //800HZ
+#define BW_400HZ       (0x0C) //400HZ
+#define BW_200HZ       (0x0B) //200HZ
+#define BW_100HZ       (0x0A) //100HZ
+#define BW_50HZ        (0x09) //50HZ
+#define BW_25HZ        (0x08) //25HZ
+#define BW_12HZ5       (0x07) //12.5HZ
+#define BW_6HZ25       (0x06) //6.25HZ
+
+//A7 is R/W Bit for address
+#define SPI_READ    (0x80) //Toggle Read
+#define SPI_WRITE   (0x00) //Toggle Write
+#define MULTI_BYTE  (0x40) //A6 is to set Multibyte R/W
+
+//FIFO Modes
+#define FIFO_BYPASS 	(0x00) //FIFO is bypassed
+#define FIFO_ON 		(0x20) //FIFO collects samples until max then stops
+#define FIFO_STREAM 	(0x40) //FIFO holds samples until buffer is full and overwrites
+#define FIFO_TRIGGER	(0xC0) //FIFO holds samples till trigger
+
+//Shock Detection
+#define SHOCK_THRESH (0x40) //~50g's
+#define SHOCK_WINDOW (0xA0) //~200ms Shock window (period of impact ~500hz)
+#define SINGLE_SHOCK (0x40) //Enable Single Shock Interrupt //defaults to INT1
+
+//Power Modes TBD
 
 /*Function Prototypes*/
 
 void adxlWrite (uint8_t address, uint8_t value);
 void adxlRead(uint8_t address, uint8_t *ptr, uint8_t size);
-void adxlIinit(void);
+void adxlInit(void);
 uint8_t adxlReadID (void);
 
-//Handle for SPI Channel 1
-SPI_HandleTypeDef spi1;
+SPI_HandleTypeDef hspi1;
 
 
 
--- Simon-Debug/Src/adxl375.c
+++ Simon-Debug/Src/adxl375.c
@@ -4,5 +4,42 @@
  *  Created on: Jun 6, 2019
  *      Author: dboles42@sdsu.edu
  */
+#include "adxl375.h"
+#include "main.h"
 
+void adxlWrite (uint8_t address, uint8_t value)
+{
+	uint8_t data[2];
+	data[0] = address|MULTI_BYTE;  // multibyte write
+	data[1] = value;
+	HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_RESET); //PULL SPI CS PIN LOW;
+	HAL_SPI_Transmit (&hspi1, data, 2, 100);
+	HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_SET); //PULL SPI CS PIN HIGH;
+}
 
+void adxlRead(uint8_t address, uint8_t *ptr, uint8_t size)
+{
+	address |= SPI_READ;  // read operation
+	address |= MULTI_BYTE;  // multibyte read
+	uint8_t data_rec;
+	HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_RESET); //PULL SPI CS PIN LOW;
+	HAL_SPI_Transmit (&hspi1, &address, 1, 100);
+	HAL_SPI_Receive (&hspi1, data_rec, 6, 100);
+	HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_SET); //PULL SPI CS PIN HIGH;
+}
+
+void adxlInit(void)
+{
+	adxlWrite (0x2d, 0x00);  // reset all bits
+	adxlWrite (0x2d, 0x08);  // power_cntl measure and wake up 8hz
+}
+uint8_t adxlReadID (void)
+{
+	uint8_t address = REG_DEVID;
+	uint8_t value;
+	HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_RESET); //PULL SPI CS PIN LOW;
+	HAL_SPI_Transmit (&hspi1, &address, 1, 100);
+	HAL_SPI_Receive (&hspi1, &value, 1, 100);
+	HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_SET); //PULL SPI CS PIN HIGH;
+	return value;
+}
